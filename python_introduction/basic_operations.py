d08e97f1ae493ee8b41d28f8ebd5829448ed4d42
Charles Ngatia <Cngatia1@gmail.com>
2024-09-13 12:08:09 +0300

    basic arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#Basic arithmetic Exercise
+number1 = 10
+number2 = 5
+
+#calculating sum, difference and product
+sum = number1 + number2
+product = number1 * number2
+difference = number1 - number2
+
+# Printing the results
+print(f"Sum: {sum}")
+print(f"Difference: {difference}")
+print(f"Product: {product}")
+
+
+
