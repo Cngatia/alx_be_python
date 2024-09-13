19897b6edc33e73357056bb58f4865ecd3bb4ff6
Charles Ngatia <Cngatia1@gmail.com>
2024-09-13 12:26:27 +0300

    simple interest calculation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Simple interest calculation
+#Interest = interest earned
+
+Principal = 1000
+Rate = 0.05
+Time = 3
+
+Interest = Principal * Rate * Time
+
+print(f"The simple interest is: ${Interest:.2f}")
