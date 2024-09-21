dbda8ddbf66a0aa30945ccd319d86faeacdb81f4
Charles Ngatia <Cngatia1@gmail.com>
2024-09-21 11:03:34 +0300

    pattern drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+# Prompt the user for the pattern size
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+
+while row < size:
+    for _ in range(size):
+        print("*", end="")
+    print()
+    row += 1
