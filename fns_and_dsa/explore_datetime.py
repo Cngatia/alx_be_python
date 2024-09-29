c8b7bf068c9f762e2a6c53b51671f23c8cfea900
Charles Ngatia <Cngatia1@gmail.com>
2024-09-30 00:19:08 +0300

    datetime edit 3

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,22 +3,22 @@ from datetime import datetime, timedelta
 def display_current_datetime():
     current_date = datetime.now()
     
-    formatted_date = current_date.strftime("YYYY-MM-DD HH:MM:SS")
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     
-    return(f"Current date and time: {formatted_date}")
+    print(f"Current date and time: {formatted_date}")
 
 def calculate_future_date(days_to_add):
     current_date = datetime.now().date()
     
     future_date = current_date + timedelta(days=days_to_add)
     
-    print(f"The date {days_to_add} days from today will be: {future_date}")
+    print(f"Future Date:{future_date:} ")
 
 def main():
     display_current_datetime()
     
     try:
-        days_to_add = int(input("Enter the number of days to add to add to the current date: "))
+        days_to_add = int(input("Enter the number of days to add to the current date: "))
 
         calculate_future_date(days_to_add)
     except ValueError:
