e2274f277bb1f649b3030db93d3fa3ce12399af9
Charles Ngatia <Cngatia1@gmail.com>
2024-09-29 21:38:53 +0300

    shopping list edit

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,13 +12,13 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            # Prompt for and add an item
+            item = input("Enter the item to add: ")
             pass
         elif choice == '2':
-            # Prompt for and remove an item
+            item = input("Enter item to remove: ")
             pass
         elif choice == '3':
-            # Display the shopping list
+            display_menu (shopping_list)
             pass
         elif choice == '4':
             print("Goodbye!")
